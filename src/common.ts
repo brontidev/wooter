fa468769d62d7823fc56b36ede8b0cf2524df68f
kai <kai@kaidoesthings.xyz>
2025-03-10 12:52:48 -0400

    Seperate NamespaceBuilder into it's own file

--- src/common.ts
+++ src/common.ts
@@ -19,3 +19,11 @@ export function defaultRouteFunction(
 		})
 	}
 }
+
+export function* concatIterators<T>(...iterators: Iterable<T, unknown, undefined>[]): Generator<T, void, undefined> {
+	for (const iterator of iterators) {
+		for (const element of iterator) {
+			yield element
+		}
+	}
+}
