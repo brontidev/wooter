7dbfd23b4ad39079f5c4dfe89757e0e1a5950720
kai <kai@kaidoesthings.xyz>
2025-09-06 13:13:15 -0400

    RouteContext is done, handler examples have been tested

---
+++ src/graph/CheminGraph.ts
@@ -0,0 +1,31 @@
+import { TChemin } from "../export/chemin.ts"
+import { InheritableCheminGraph } from "./InheritableCheminGraph.ts"
+
+/**
+ * Constructable
+ */
+export class CheminGraph<
+	Node,
+	FindData,
+> extends InheritableCheminGraph<Node, FindData> {
+	/**
+	 * Matches a node from a pathname
+	 * @param pathname Pathname or Path parts
+	 * @param data Extra check data
+	 * @returns Node Definition
+	 */
+	override getNode(
+		pathname: string | string[],
+		data: FindData,
+	): { params: unknown; node: Node } | undefined {
+		return super.getNode(pathname, data)
+	}
+	/**
+	 * Adds a node to the graph
+	 * @param path Chemin to match
+	 * @param node Node
+	 */
+	override pushNode(path: TChemin, node: Node): void {
+		super.pushNode(path, node)
+	}
+}
