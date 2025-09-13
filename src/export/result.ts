59586fb61cf3a0107c4887a5d47049a4f303991f
kai <kai@kaidoesthings.xyz>
2025-09-13 09:26:06 -0400

    Modify option & result exports

--- src/export/result.ts
+++ src/export/result.ts
@@ -4,4 +4,14 @@
  * @module
  */
 
+ import * as _res from "@oxi/result"
+
+ /**
+  * All @oxi/result exports
+  */
+ const res: typeof import("@oxi/result") = _res
+
+ export default res
+
+
 export * from "@oxi/result"
