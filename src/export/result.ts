430d148b714c75c00379094327484fe9852747ce
kai <kai@kaidoesthings.xyz>
2025-09-13 09:34:39 -0400

    Fix option and result exports

--- src/export/result.ts
+++ src/export/result.ts
@@ -4,7 +4,13 @@
  * @module
  */
 
+import * as _res from "@oxi/result"
 
+/**
+ * All @oxi/result exports
+ */
+const res: typeof import("@oxi/result") = _res
 
+export default res
 
 export * from "@oxi/result"
