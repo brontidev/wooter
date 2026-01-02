e22efe17c9d79d957198c425ab11e83704854b0b
cleo <kai@kaidoesthings.xyz>
2026-01-01 21:26:14 -0500

    Re-write option & result exports

--- src/export/result.ts
+++ src/export/result.ts
@@ -4,13 +4,4 @@
  * @module
  */
 
-import * as _res from "@oxi/result"
-
-/**
- * All @oxi/result exports
- */
-const res: typeof import("@oxi/result") = _res
-
-export default res
-
 export * from "@oxi/result"
