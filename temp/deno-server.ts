89af4ce09fe2d64617e54d82151b76c7d229962f
cleo <kai@kaidoesthings.xyz>
2026-01-01 19:58:46 -0500

    Format

--- temp/deno-server.ts
+++ temp/deno-server.ts
@@ -1,33 +1,39 @@
-import { Wooter, c } from "@@/index.ts"
+import { c, Wooter } from "@@/index.ts"
 
-const wooter = new Wooter();
-wooter.route(c.chemin("min"), "GET", async ctx => {
-    const file = await Deno.readFile("./out/index.m.js")
-    ctx.resp(new Response(file, {
-        headers: {
-            "Content-Type": "text/javascript",
-            "X-Typescript-Types": "./index.d.ts"
-        }
-    }))
+const wooter = new Wooter()
+wooter.route(c.chemin("min"), "GET", async (ctx) => {
+	const file = await Deno.readFile("./out/index.m.js")
+	ctx.resp(
+		new Response(file, {
+			headers: {
+				"Content-Type": "text/javascript",
+				"X-Typescript-Types": "./index.d.ts",
+			},
+		}),
+	)
 })
 
-wooter.route(c.chemin(), "GET", async ctx => {
-    const file = await Deno.readFile("./out/index.js")
-    ctx.resp(new Response(file, {
-        headers: {
-            "Content-Type": "text/javascript",
-            "X-Typescript-Types": "./index.d.ts"
-        }
-    }))
+wooter.route(c.chemin(), "GET", async (ctx) => {
+	const file = await Deno.readFile("./out/index.js")
+	ctx.resp(
+		new Response(file, {
+			headers: {
+				"Content-Type": "text/javascript",
+				"X-Typescript-Types": "./index.d.ts",
+			},
+		}),
+	)
 })
 
-wooter.route(c.chemin("index.d.ts"), "GET", async ctx => {
-    const file = await Deno.readFile("./out/index.d.ts")
-    ctx.resp(new Response(file, {
-        headers: {
-            "Content-Type": "text/typescript"
-        }
-    }))
+wooter.route(c.chemin("index.d.ts"), "GET", async (ctx) => {
+	const file = await Deno.readFile("./out/index.d.ts")
+	ctx.resp(
+		new Response(file, {
+			headers: {
+				"Content-Type": "text/typescript",
+			},
+		}),
+	)
 })
 
-export default wooter;
+export default wooter
