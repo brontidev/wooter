8ca110189a4a8de14ba0fd480b2c6f7dd01a5515
cleo <kai@kaidoesthings.xyz>
2026-01-01 23:50:58 -0500

    Fix lint errors

--- src/export/response.ts
+++ src/export/response.ts
@@ -1,3 +1,8 @@
+/**
+ * Creates a redirect Response
+ * @param location Location to redirect to
+ * @param init Request init object
+ */
 export function makeRedirect(location: string | URL, init?: ResponseInit): Response {
 	const headers = new Headers(init?.headers)
 	headers.set("Location", location.toString())
@@ -8,6 +13,12 @@ export function makeRedirect(location: string | URL, init?: ResponseInit): Respo
 	})
 }
 
+/**
+ * Creates an error Response
+ * @param status status code
+ * @param message Error message
+ * @param headers Request headers
+ */
 export function makeError(status: number, message?: string, headers?: HeadersInit): Response {
 	return new Response(message, {
 		status,
