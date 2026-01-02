e792445fdb4936d50443c4511f96609d364fd296
cleo <kai@kaidoesthings.xyz>
2026-01-01 19:58:46 -0500

    Create /use and /response utilities

---
+++ src/export/use.ts
@@ -0,0 +1,32 @@
+// TODO: Create a function that transforms a handler by applying middleware to it on the spot
+
+import { Data, MiddlewareHandler, OptionalMerge, Params, RouteHandler } from "@@/types.ts"
+import MiddlewareContext from "@/ctx/MiddlewareContext.ts"
+import RouteContext, { RouteContext__block, RouteContext__respond } from "@/ctx/RouteContext.ts"
+
+/**
+ * Applies a middleware directly to a handler, returning a new handler
+ * @param middlewareHandler - Middleware to apply
+ * @param handler - Handler to apply middleware to
+ * @returns Handler - Handler that wraps the handler in the middleware
+ */
+export default function use<
+	TParams extends Params,
+	BaseData extends Data,
+	NextData extends Data | undefined = undefined,
+>(
+	middlewareHandler: MiddlewareHandler<TParams, BaseData, NextData>,
+	handler: RouteHandler<TParams, OptionalMerge<Data, BaseData, NextData>>,
+): RouteHandler<TParams, BaseData> {
+	return ({ params: _params, request, data, resp }) => {
+		const params = Object.fromEntries(_params.entries())
+		const internalHandler = MiddlewareContext.useMiddlewareHandler(middlewareHandler, params, (data, request) => {
+			return RouteContext.useRouteHandler(handler, params)(data, request)
+		})
+
+		const new_ctx = internalHandler(Object.fromEntries(data.entries()), request)
+
+		new_ctx[RouteContext__respond].promise.then((v) => v.map((r) => resp(r)))
+		return new_ctx[RouteContext__block].promise
+	}
+}
