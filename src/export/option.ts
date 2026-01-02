386e0c68058b8c7d21750280ba69e8fd9a58dbdc
cleo <kai@kaidoesthings.xyz>
2026-01-01 21:26:14 -0500

    Re-write option & result exports

--- src/export/option.ts
+++ src/export/option.ts
@@ -4,13 +4,4 @@
  * @module
  */
 
-import * as _op from "@oxi/option"
-
-/**
- * All @oxi/option exports
- */
-const op: typeof import("@oxi/option") = _op
-
-export default op
-
 export * from "@oxi/option"
